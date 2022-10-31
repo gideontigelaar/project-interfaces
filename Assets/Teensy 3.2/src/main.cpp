85a4374cf74b068f880168d132c91dbc8f5e23eb
Gideon <gidtigelaar@gmail.com>
2022-10-31 13:46:06 +0100

    made POTMeter work

--- Assets/Teensy 3.2/src/main.cpp
+++ Assets/Teensy 3.2/src/main.cpp
@@ -3,14 +3,18 @@
 int potPin = A0;
 int potValue = 0;
 
-void setup() {
-  // put your setup code here, to run once:
+void setup() { // put your setup code here, to run once:
   pinMode(potPin, INPUT);
   Serial.begin(115200);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  potValue = analogRead(potPin);
-  Serial.println(potValue);
+void loop() { // put your main code here, to run repeatedly:
+  if(Serial.available()) {
+    char inByte = Serial.read();
+
+    if(inByte == 'a') {
+      int potValue = analogRead(potPin);
+      Serial.println(potValue);
+    }
+  }
 }
